cb79a42507df0363a8c0716928093a32f6d965d6
niki <niku17patel@gmail.com>
2023-01-25 16:04:15 +0530

    data insert in database

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
-
+import sqlite3
 import requests
 import selectorlib
-import smtplib,ssl
+import smtplib, ssl
 import os
+
+connection = sqlite3.connect("data.db")
 
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
@@ -39,21 +41,30 @@ def send_email(message):
     print("Email was sent!")
 
 def store(extracted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", row)
+    connection.commit()
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-       return file.read()
+    row = extracted.split(",")
+    row = [item.strip() for item in row]
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
 if __name__=="__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
         print(extracted)
-        content = read(extracted)
+
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            if not row:
                 store(extracted)
                 send_email(message="Hey! New event was found")
         time.sleep(2)
